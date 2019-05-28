8972f5a7504582f75d1b753f1e606875eb16496b
Konrad Szwarc <konrad.szwarc@outlook.com>
2019-05-28 14:51:16 +0200

    Recipes queries and mutations

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -11,6 +11,18 @@ const Query = {
     const user = await ctx.prisma.query.user({ where: { id: ctx.request.userId } }, info);
     return user;
   },
+  recipes: async (parent, args, ctx, info) => {
+    const recipes = await ctx.prisma.query.recipes(args, info);
+    return recipes;
+  },
+  recipe: async (parent, args, ctx, info) => {
+    const recipe = await ctx.prisma.query.recipe(args, info);
+    return recipe;
+  },
+  myRecipes: async (parent, args, ctx, info) => {
+    const recipes = ctx.prisma.query.recipes({ where: { user: { id: ctx.request.userId } } });
+    return recipes;
+  },
 };
 
 module.exports = Query;
