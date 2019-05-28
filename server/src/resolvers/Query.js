85c453a606446a614fac90e0f6bf375f944177aa
Konrad Szwarc <konrad.szwarc@outlook.com>
2019-05-28 14:12:50 +0200

    User authorization and authentication

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -1,3 +1,16 @@
-const Query = {};
+const Query = {
+  users: async (parent, args, ctx, info) => {
+    const users = await ctx.prisma.query.users(args, info);
+    return users;
+  },
+  me: async (parent, args, ctx, info) => {
+    if (!ctx.request.userId) {
+      return null;
+    }
+
+    const user = await ctx.prisma.query.user({ where: { id: ctx.request.userId } }, info);
+    return user;
+  },
+};
 
 module.exports = Query;
